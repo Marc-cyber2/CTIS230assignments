a820b44075370d60d150f8736048b00ef8e63d11
Marc-cyber2 <manang@guilford.edu>
2025-03-13 09:52:41 -0400

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -2,7 +2,7 @@ myitem = document.getElementById("firsttest");
 
 myitem.addEventListener("click", onClick);
 
-myitem.addEventListener("click", onmousemove);
+myitem.addEventListener("move", onmousemove);
 function onClick() {  
     myitem.style.color = "black";  
     myitem.style.fontSize = "30px"
