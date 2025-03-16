f70833350d3458b6792fd57ed148d07de1d81390
Marc-cyber2 <manang@guilford.edu>
2025-03-16 08:03:07 -0400

    update

--- testscript.js
+++ testscript.js
@@ -13,3 +13,15 @@ function onmousemove() {
         myitem.style.color = "darkkahki";  
         myitem.style.fontSize = "1em"
 }
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+thebutton.addEventListener("click", onButtonClick);
+function onButtonClick() {  
+    otheritem.style.color = "red";  
+}
+textentry = document.getElementById("myinput");  
+textentry.addEventListener("change", onChange);
+function onChange() {  
+    newtext = myinput.value;  
+    otheritem.innerHTML = newtext;  
+}
