2e06feeb583120c69b47e1f0639f1abb9833b260
Marc-cyber2 <manang@guilford.edu>
2025-03-26 10:28:43 -0400

    update

--- testscript.js
+++ testscript.js
@@ -4,21 +4,23 @@ myitem.addEventListener("click", onClick);
 
 function onClick() {  
     myitem.style.color = "purple";  
-    myitem.style.fontSize = "30px"
+    myitem.style.fontSize = "20px"
 }
 
 myitem.addEventListener("mouseout", onmousemove);
 
 function onmousemove() {  
-        myitem.style.color = "darkkahki";  
+        myitem.style.color = "black";  
         myitem.style.fontSize = "1em"
 }
+
 thebutton = document.getElementById("thebutton");  
 otheritem = document.getElementById("buttontest");
 thebutton.addEventListener("click", onButtonClick);
 function onButtonClick() {  
     otheritem.style.color = "red";  
 }
+
 textentry = document.getElementById("myinput");  
 textentry.addEventListener("change", onChange);
 function onChange() {  
